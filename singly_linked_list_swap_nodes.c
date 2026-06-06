9ad60d2a35c76e9db1a32f5f51bbc6182af49981
hapepo23 <238172935+hapepo23@users.noreply.github.com>
2026-06-06 15:19:48 +0200

    update 2026-06-06 15:19 CEST

--- singly_linked_list_swap_nodes.c
+++ singly_linked_list_swap_nodes.c
@@ -4,10 +4,7 @@ Singly Linked List - Swap Nodes
 https://www.codewars.com/kata/58d91d5f024c724b50000300
 */
 
-#include <stdio.h>
 #include <stdlib.h>
-
-#define DEBUG 0
 
 typedef struct Node {
   int value;
@@ -16,50 +13,39 @@ typedef struct Node {
 typedef Node* LinkedList;
 typedef LinkedList* PLinkedList;
 
-char* listToString(LinkedList list);
-
 int swapNodes(PLinkedList listPointer1,
               unsigned int index1,
               PLinkedList listPointer2,
               unsigned int index2) {
-  if (DEBUG) {
-    char* list1str = listToString(*listPointer1);
-    char* list2str = listToString(*listPointer2);
-    printf("List 1 (%d):\t%s\nList 2 (%d):\t%s\n", index1, list1str, index2,
-           list2str);
-    fflush(stdout);
-    free(list1str);
-    free(list2str);
-  }
   LinkedList list1 = *listPointer1;
   LinkedList plist1 = NULL;
-  unsigned int i1 = -1;
+  int i1 = -1;
   while (list1 != NULL) {
     i1++;
-    if (index1 == i1)
+    if ((int)index1 == i1)
       break;
     plist1 = list1;
     list1 = list1->next;
   }
-  if (i1 != index1)
+  if (i1 != (int)index1)
     return 0;
   LinkedList list2 = *listPointer2;
   LinkedList plist2 = NULL;
-  unsigned int i2 = -1;
+  int i2 = -1;
   while (list2 != NULL) {
     i2++;
-    if (index2 == i2)
+    if ((int)index2 == i2)
       break;
     plist2 = list2;
     list2 = list2->next;
   }
-  if (i2 != index2)
+  if (i2 != (int)index2)
     return 0;
   // Swap next
   LinkedList savenext = list1->next;
   list1->next = list2->next;
   list2->next = savenext;
-  // Swap prev->next or start
+  // Swap prev->next or startptr
   PLinkedList pprevnext1 = plist1 == NULL ? listPointer1 : &(plist1->next);
   PLinkedList pprevnext2 = plist2 == NULL ? listPointer2 : &(plist2->next);
   LinkedList saveprevnext = *pprevnext1;
