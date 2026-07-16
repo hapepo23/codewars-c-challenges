86d1e95434e2b85a10d37f3e2195f2a274af06bd
hapepo23 <238172935+hapepo23@users.noreply.github.com>
2026-07-16 21:22:42 +0200

    update 2026-07-16 21:22 CEST

--- collinearity.c
+++ collinearity.c
@@ -12,5 +12,5 @@ bool collinearity(int x1, int y1, int x2, int y2) {
     return true;
   if ((x1 == 0 && x2 != 0) || (y1 == 0 && y2 != 0))
     return false;
-  return fabs((double)x2 / (double)x1 * (double)y1 - (double)y2) < 1e-5;
+  return fabs((double)x2 / (double)x1 * (double)y1 - (double)y2) < 1e-10;
 }
